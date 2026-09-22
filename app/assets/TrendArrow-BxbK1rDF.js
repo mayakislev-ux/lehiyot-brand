a8b4b8c99bb2a995844ee7766738571d76ef7e00
Maya Kislev <mayakislev@gmail.com>
2026-09-22 15:58:53 +0300

    Portal copy on the personal domain: update

---
+++ app/assets/TrendArrow-BxbK1rDF.js
@@ -0,0 +1,1 @@
+import{bt as e}from"./index-DV-inA2T.js";var t=e();function n({trend:e}){return e===`up`?(0,t.jsx)(`span`,{style:{color:`var(--success)`},children:`▲`}):e===`down`?(0,t.jsx)(`span`,{style:{color:`var(--danger)`},children:`▼`}):null}export{n as t};
